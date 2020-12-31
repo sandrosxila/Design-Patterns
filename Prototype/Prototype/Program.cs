5301cc8f9b00b33e13888043bcc97e74ff8731d4
sandrosxila <sandrosxila@yahoo.com>
2020-12-31 04:01:45 +0400

    prototype pattern added

---
+++ Prototype/Prototype/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace Prototype
+{
+    public static class ExtensionMethods
+    {
+        public static T DeepCopy<T>(this T self)
+        {
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, self);
+            stream.Seek(0, SeekOrigin.Begin);
+            object copy = formatter.Deserialize(stream);
+            stream.Close();
+            return (T) copy;
+        }
+
+        public static T DeepCopyXml<T>(this T self)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var s = new XmlSerializer(typeof(T));
+                s.Serialize(ms, self);
+                ms.Position = 0;
+                return (T) s.Deserialize(ms);
+            }
+        }
+    }
+
+    [Serializable]
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person()
+        {
+            
+        }
+        
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+    }
+
+    [Serializable]
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address()
+        {
+            
+        }
+        
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var john = new Person(new[] {"John", "Smith"}, new Address("London Road", 1553));
+            var jane = john.DeepCopy();
+            jane.Names[0] = "jane";
+            jane.Address.HouseNumber = 450;
+            Console.WriteLine(john);
+            Console.WriteLine(jane);
+        }
+    }
+}
