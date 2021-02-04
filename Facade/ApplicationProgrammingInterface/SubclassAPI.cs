f5613fb79ced41a986467475a0e970e300537d48
sandrosxila <sandrosxila@yahoo.com>
2021-02-04 15:58:57 +0400

    Facade Pattern

---
+++ Facade/ApplicationProgrammingInterface/SubclassAPI.cs
@@ -0,0 +1,45 @@
+﻿namespace ApplicationProgrammingInterface
+{
+    internal interface ISubclass
+    {
+        string Output();
+    }
+
+    internal class SubclassA : ISubclass
+    {
+        public string Output()
+        {
+            return new string('A', 1);
+        }
+    }
+
+    internal class SubclassB : ISubclass
+    {
+        public string Output()
+        {
+            return new string('B', 1);
+            ;
+        }
+    }
+
+    internal class SubclassC : ISubclass
+    {
+        public string Output()
+        {
+            return new string('C', 1);
+            ;
+        }
+    }
+
+    public class SubclassAPI
+    {
+        public string Result()
+        {
+            var a = new SubclassA();
+            var b = new SubclassB();
+            var c = new SubclassC();
+            var result = a.Output() + b.Output() + c.Output();
+            return result;
+        }
+    }
+}
---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ApplicationProgrammingInterface;
+
+namespace Facade
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var r = new SubclassAPI().Result();
+            Console.WriteLine(r);
+        }
+    }
+}
