ceb33dd8e76472fec6362f12830aaa5d45791644
sandrosxila <sandrosxila@yahoo.com>
2021-02-05 21:41:50 +0400

    Proxy Pattern

---
+++ Proxy/PropertyProxy/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PropertyProxy
+{
+    public class Property<T> : IEquatable<Property<T>> where T : new()
+    {
+        private T value;
+
+        public T Value
+        {
+            get => this.value;
+            set
+            {
+                if (Equals(this.value, value))
+                    return;
+                Console.WriteLine($"Assigning value to {value}");
+                this.value = value;
+            }
+        }
+
+        public Property() : this(default(T))
+        {
+        }
+
+        public Property(T value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator T(Property<T> property)
+        {
+            return property.value;
+        }
+
+        public static implicit operator Property<T>(T value)
+        {
+            return new Property<T>(value);
+        }
+
+        public bool Equals(Property<T> other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return EqualityComparer<T>.Default.Equals(value, other.value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Property<T>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<T>.Default.GetHashCode(value);
+        }
+
+        public static bool operator ==(Property<T> left, Property<T> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Property<T> left, Property<T> right)
+        {
+            return !Equals(left, right);
+        }
+    }
+
+    public class Creature
+    {
+        private Property<int> agility = new Property<int>();
+
+        public int Agility
+        {
+            get => agility.Value;
+            set => agility.Value = value;
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var c = new Creature();
+            c.Agility = 10;
+            c.Agility = 10;
+        }
+    }
+}
---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Proxy
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("The car is being driven...");
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+
+        public Driver(int age)
+        {
+            Age = age;
+        }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Driver driver;
+        private Car car = new Car();
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+        
+        public void Drive()
+        {
+            if(driver.Age >= 16)
+                car.Drive();
+            else
+                Console.WriteLine("Too young");
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ICar carProxy = new CarProxy(new Driver(22));
+            carProxy.Drive();
+        }
+    }
+}
